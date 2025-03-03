4aa877f782f42cdeba8491a50cee6eeb677909aa
Ashley Butcher <ashley@alien.net.au>
2025-03-03 22:21:51 +0000

    Boring boilerplate stuff

---
+++ ais_url_alias.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ais_url_alias - URL aliases for Textpattern
+ *
+ * Technically it's a URI alias plugin since it only really impacts the path bit
+ * of the URL, but everyone calls the functionality 'URL alias' for some reason, 
+ * which makes the nerd in me very sad.
+ * 
+ * Copyright (C) 2025 Ashley Butcher (Alien Internet Services)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author	Ashley Butcher (Alien Internet Services)
+ * @copyright   Copyright (C) 2025 Ashley Butcher (Alien Internet Services)
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3
+ * @version	0.1
+ * @link	https://github.com/alieninternet/ais_url_alias/
+ */
+
+
+// Test mode of operation
+switch (txpinterface) {
+ case 'admin':
+    // TODO: This
+    break;
+    
+ case 'public':
+    // TODO: This
+    break;
+}
